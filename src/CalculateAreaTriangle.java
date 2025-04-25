dd2753d290b0b269926a0e1626ca7eee472c7730
neosbamen <lorzproducciones@gmail.com>
2025-04-25 01:39:41 -1200

    apdate de calcular area triangulo

---
+++ src/CalculateAreaTriangle.java
@@ -0,0 +1,7 @@
+public class CalculateAreaTriangle {
+
+    public int calculate(int base, int height){
+
+        return base*height/2;
+    };
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Scanner scanner =new Scanner(System.in);
+        CalculateAreaTriangle calculateAreaTriangle=new CalculateAreaTriangle();
+
+        System.out.println("ingresa la base del triangulo");
+        int base=scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("ingresa la altura del triangulo");
+        int altura=scanner.nextInt();
+        scanner.close();
+        int result=calculateAreaTriangle.calculate(base,altura);
+
+        System.out.println(result);
     }
 }
