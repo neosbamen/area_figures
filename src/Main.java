31b57b6b20eca6b7b021bb799d19bbba944c9039
Kirutasu <125732333+Kirutasu@users.noreply.github.com>
2025-04-26 19:25:34 +0200

    Añadida area rectangulo, clase+metodo

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,42 @@
 import java.util.Scanner;
 
 
-public class Main extends  AreaCuadrado{
+public class Main extends AreaCuadrado {
     public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in);
-      CalculateAreaTriangle calculateAreaTriangle=new CalculateAreaTriangle();
 
+        Scanner scanner = new Scanner(System.in);
+        CalculateAreaTriangle calculateAreaTriangle = new CalculateAreaTriangle();
+        AreaRectangulo areaRectangulo = new AreaRectangulo();
         int lado;
+        int baseRect;
+        int alturaRect;
+        int resultadoRectangulo;
 
         System.out.println("Dime el lado del cuadrado cuya area quieres calcular");
         lado = scanner.nextInt();
         scanner.nextLine();
-      
         areaCuadrado(lado);
 
         System.out.println("ingresa la base del triangulo");
-        int base=scanner.nextInt();
+        int base = scanner.nextInt();
         scanner.nextLine();
 
         System.out.println("ingresa la altura del triangulo");
-        int altura=scanner.nextInt();
-        scanner.close();
-        int result=calculateAreaTriangle.calculate(base,altura);
+        int altura = scanner.nextInt();
+        int result = calculateAreaTriangle.calculate(base, altura);
 
         System.out.println(result);
-        }
-        }
+
+        System.out.println("Introduce la base del rectangulo cuya area quieres calcular");
+        baseRect = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Introduce la altura del rectangulo cuya area quieres calcular");
+        alturaRect = scanner.nextInt();
+        scanner.nextLine();
+        resultadoRectangulo = areaRectangulo.areaRectangulo(baseRect, alturaRect);
+        System.out.println("El area del rectangulo de base: " + baseRect + " y altura: " + alturaRect
+                + " es: " + resultadoRectangulo);
+
+        scanner.close();
+    }
+}
